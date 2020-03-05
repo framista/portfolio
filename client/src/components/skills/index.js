84f42a6b4f9f4a653fdfd6799f4e5981107e9b2e
framista <adajab1@gmail.com>
2020-03-05 23:55:15 +0100

    Create layout for skills

--- client/src/components/skills/index.js
+++ client/src/components/skills/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import "./style.css"
+import { Icon } from '@iconify/react';
+import javascriptIcon from '@iconify/icons-logos/javascript';
+import html5 from '@iconify/icons-logos/html-5';
+import css3 from '@iconify/icons-logos/css-3';
+import bootstrapIcon from '@iconify/icons-logos/bootstrap';
+import reactIcon from '@iconify/icons-logos/react';
+import nodejsIcon from '@iconify/icons-logos/nodejs';
+import mongodbIcon from '@iconify/icons-logos/mongodb';
+import sqliteIcon from '@iconify/icons-logos/sqlite';
+import webpackIcon from '@iconify/icons-logos/webpack';
+import gitIcon from '@iconify/icons-logos/git';
+
 
 const Skills = () => {
     return (
-        <div className="container">Skills</div>
+        <div className="skills__container">
+            <Icon className="skills__icon" icon={javascriptIcon} />
+            <Icon className="skills__icon" icon={reactIcon} />
+            <Icon className="skills__icon" icon={webpackIcon} />
+            <Icon className="skills__icon" icon={html5} />
+            <Icon className="skills__icon" icon={css3} />
+            <Icon className="skills__icon" icon={bootstrapIcon} />
+            <Icon className="skills__icon" icon={nodejsIcon} />
+            <Icon className="skills__icon" icon={mongodbIcon} />
+            <Icon className="skills__icon" icon={sqliteIcon} />
+            <Icon className="skills__icon" icon={gitIcon} />
+        </div>
     )
 }
 
